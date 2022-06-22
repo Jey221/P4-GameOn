19392c7497dfe1392669a5eb9cff752854be6f1d
jeremy koundour <j.koundour@gmail.com>
2022-06-22 21:54:37 +0200

    Ajout fonctionnalités au bouton x

--- GameOn-website-FR/starterOnly/modal.js
+++ GameOn-website-FR/starterOnly/modal.js
@@ -11,6 +11,7 @@ function editNav() {
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
+let closeBtn = document.querySelectorAll(".close-btn");
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
@@ -19,5 +20,9 @@ modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
 function launchModal() {
   modalbg.style.display = "block";
 }
-
+function launchModal2() {
+  modalbg.style.display = "none";
+}
+// close button event 
+  closeBtn.forEach((btn) => btn.addEventListener("click", launchModal2))
 
