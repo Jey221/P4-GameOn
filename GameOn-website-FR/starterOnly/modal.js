f78f0590b351b6e22e39ff8875968cff3483b70c
jeremy koundour <j.koundour@gmail.com>
2022-06-28 16:00:32 +0200

    mise en place de commentaire pour codes js // création nouvelle branche

--- GameOn-website-FR/starterOnly/modal.js
+++ GameOn-website-FR/starterOnly/modal.js
@@ -11,9 +11,9 @@ function editNav() {
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
-let closeBtn = document.querySelectorAll(".close-btn");
-let data = document.querySelectorAll(".formData");
-let checkboxLocation = document.querySelectorAll(".checkbox-input");
+let closeBtn = document.querySelectorAll(".close-btn");// mise en place d'une variable pour bouton X
+let data = document.querySelectorAll(".formData");// mise en place d'une variable pour le formulaire
+let checkboxLocation = document.querySelectorAll(".checkbox-input");// mise en place d'une variable pour bouton location
 let valid = true;
 
 // launch modal event
@@ -26,7 +26,7 @@ function launchModal() {
 function launchModal2() {
   modalbg.style.display = "none";
 }
-// close button event 
+// close button event  // fonctionnalité du bouton X 
 closeBtn.forEach((btn) => btn.addEventListener("click", launchModal2))
 // validate form
 console.log(checkboxLocation)
