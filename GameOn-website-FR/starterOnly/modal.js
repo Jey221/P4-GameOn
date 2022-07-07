4c6e641820e53a68590bf9396668eed07a69021f
jeremy koundour <j.koundour@gmail.com>
2022-07-07 22:41:53 +0200

    mise en place des input rouge si error

--- GameOn-website-FR/starterOnly/modal.js
+++ GameOn-website-FR/starterOnly/modal.js
@@ -12,7 +12,7 @@ const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
 let closeBtn = document.querySelectorAll(".close-btn");// mise en place d'une variable pour bouton X
-let merci = document.querySelectorAll(".merci-page")
+
 
 
 // launch modal event
@@ -35,14 +35,15 @@ closeBtn.forEach((btn) => btn.addEventListener("click", launchModal2))
 //let fields = document.querySelectorAll('input[required]');// mise en place d'une variable pour les diff element à valider
 //let valid =true;
 
+
 function validate()                                    
-{ 
-    let firstName = document.forms["reserve"]["first"];
-    let lastName = document.forms["reserve"]["last"];               
-    let email = document.forms["reserve"]["email"];      
-    let birthdate=  document.forms["reserve"]["birthdate"];  
-    let quantity= document.forms["reserve"]["quantity"];  
-    let checkbox= document.forms["reserve"]["checkbox1"]; 
+{   
+    let firstName = document.getElementById('first');
+    let lastName = document.getElementById('last');               
+    let email = document.getElementById('email');      
+    let birthdate=  document.getElementById('birthdate');  
+    let quantity= document.getElementById('quantity');  
+    let checkbox= document.getElementById('checkbox1'); 
     let smallFirst = firstName.nextElementSibling;
     let smallLast = lastName.nextElementSibling;
     let smallEmail = email.nextElementSibling;
@@ -51,92 +52,96 @@ function validate()
     let smallCheckbox = document.getElementById('errorCheckbox');
     let smallCheckboxCity = document.getElementById('errorCheckboxCity')
     let radioInput = document.querySelectorAll('.radio-input')
-    const isRadioChecked = Array.from(radioInput).filter(
-      (radioBtn) => radioBtn.checked
-    );
+    const isRadioChecked = Array.from(radioInput).filter((radioBtn) => radioBtn.checked);
+
 
     if (firstName.value.length < 2)                                  
     { 
-        smallFirst.innerHTML = "Veuillez entrer 2 caractères ou plus pour le champ prénom.";
-        firstName.focus(); 
-        return false; 
+      smallFirst.innerHTML = "Veuillez entrer 2 caractères ou plus pour le champ prénom.";
+      
+      firstName.classList.add("error");
+      firstName.focus(); 
+      return false; 
     }  
     if (lastName.value.length < 2)                                  
     { 
-        smallFirst.innerHTML = "";
-        smallLast.innerHTML = "Veuillez entrer 2 caractères ou plus pour le champ nom.";
-        lastName.focus(); 
-        return false; 
+      smallFirst.innerHTML = "";
+      smallLast.innerHTML = "Veuillez entrer 2 caractères ou plus pour le champ nom.";
+      lastName.classList.add("error");
+      firstName.classList.remove("error");
+      lastName.focus(); 
+      return false; 
     }   
     
+
     if (email.value == "")                                   
     {   
-        smallFirst.innerHTML = "";
-        smallLast.innerHTML = "";
-        smallEmail.innerHTML= "Vous devez entrer une adresse email valide."; 
-        email.focus(); 
-        return false; 
+      smallFirst.innerHTML = "";
+      smallLast.innerHTML = "";
+      smallEmail.innerHTML= "Vous devez entrer une adresse email valide."; 
+      email.focus(); 
+      return false; 
     }    
     if (email.value.indexOf("@", 0) < 0)                 
     { 
-        smallFirst.innerHTML = "";
-        smallLast.innerHTML = "";
-        smallEmail.innerHTML= "Vous devez entrer une adresse email valide."; 
-        email.focus(); 
-        return false; 
+      smallFirst.innerHTML = "";
+      smallLast.innerHTML = "";
+      smallEmail.innerHTML= "Vous devez entrer une adresse email valide."; 
+      email.focus(); 
+      return false; 
     }    
     if (email.value.indexOf(".", 0) < 0)                 
     { 
-        smallFirst.innerHTML = "";
-        smallLast.innerHTML = "";
-        smallEmail.innerHTML = "Vous devez entrer une adresse email valide."; 
-        email.focus(); 
-        return false; 
+      smallFirst.innerHTML = "";
+      smallLast.innerHTML = "";
+      smallEmail.innerHTML = "Vous devez entrer une adresse email valide."; 
+      email.focus(); 
+      return false; 
     }    
     if (birthdate.value == "")                           
     { 
-        smallFirst.innerHTML = "";
-        smallLast.innerHTML = "";
-        smallEmail.innerHTML = "";
-        smallBirthdate.innerHTML = "Vous devez entrer votre date de naissance."; 
-        birthdate.focus(); 
-        return false; 
+      smallFirst.innerHTML = "";
+      smallLast.innerHTML = "";
+      smallEmail.innerHTML = "";
+      smallBirthdate.innerHTML = "Vous devez entrer votre date de naissance."; 
+      birthdate.focus(); 
+      return false; 
     }    
     if (quantity.value == "")                        
     { 
-        smallFirst.innerHTML = "";
-        smallLast.innerHTML = "";
-        smallEmail.innerHTML = "";
-        smallBirthdate.innerHTML = "";
-        smallQuantity.innerHTML = "Vous devez mettre une valeur."; 
-        quantity.focus(); 
-        return false; 
+      smallFirst.innerHTML = "";
+      smallLast.innerHTML = "";
+      smallEmail.innerHTML = "";
+      smallBirthdate.innerHTML = "";
+      smallQuantity.innerHTML = "Vous devez mettre une valeur."; 
+      quantity.focus(); 
+      return false; 
     }  
     if (!isRadioChecked || isRadioChecked.length <= 0) 
     {
-        smallFirst.innerHTML = "";
-        smallLast.innerHTML = "";
-        smallEmail.innerHTML = "";
-        smallBirthdate.innerHTML = "";
-        smallQuantity.innerHTML = "";
-        smallCheckboxCity.innerHTML = "Sélectionnez une ville"; 
-        checkbox.focus()
-        return false;
+      smallFirst.innerHTML = "";
+      smallLast.innerHTML = "";
+      smallEmail.innerHTML = "";
+      smallBirthdate.innerHTML = "";
+      smallQuantity.innerHTML = "";
+      smallCheckboxCity.innerHTML = "Sélectionnez une ville"; 
+      checkbox.focus()
+      return false;
     }  
     if (!checkbox.checked) 
     {
-        smallFirst.innerHTML = "";
-        smallLast.innerHTML = "";
-        smallEmail.innerHTML = "";
-        smallBirthdate.innerHTML = "";
-        smallQuantity.innerHTML = "";
-        smallCheckboxCity.innerHTML = "";
-        smallCheckbox.innerHTML = "Vérifier que vous avez accepter les termes et conditions."; 
-        checkbox.focus()
-        return false;
+      smallFirst.innerHTML = "";
+      smallLast.innerHTML = "";
+      smallEmail.innerHTML = "";
+      smallBirthdate.innerHTML = "";
+      smallQuantity.innerHTML = "";
+      smallCheckboxCity.innerHTML = "";
+      smallCheckbox.innerHTML = "Vérifier que vous avez accepter les termes et conditions."; 
+      checkbox.focus()
+      return false;
     }
 
 
-    return true;
+return true;
      
 }
