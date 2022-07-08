c026055aba398baf6ebe89a7819e84275ffdfe5d
jeremy koundour <j.koundour@gmail.com>
2022-07-08 15:06:59 +0200

    nvelle techn

--- GameOn-website-FR/starterOnly/modal.js
+++ GameOn-website-FR/starterOnly/modal.js
@@ -36,111 +36,23 @@ closeBtn.forEach((btn) => btn.addEventListener("click", launchModal2))
 //let valid =true;
 
 
-function validate()                                    
-{   
-    let firstName = document.getElementById('first');
-    let lastName = document.getElementById('last');               
-    let email = document.getElementById('email');      
-    let birthdate=  document.getElementById('birthdate');  
-    let quantity= document.getElementById('quantity');  
-    let checkbox= document.getElementById('checkbox1'); 
-    let smallFirst = firstName.nextElementSibling;
-    let smallLast = lastName.nextElementSibling;
-    let smallEmail = email.nextElementSibling;
-    let smallBirthdate = birthdate.nextElementSibling;
-    let smallQuantity = quantity.nextElementSibling;
-    let smallCheckbox = document.getElementById('errorCheckbox');
-    let smallCheckboxCity = document.getElementById('errorCheckboxCity')
-    let radioInput = document.querySelectorAll('.radio-input')
-    const isRadioChecked = Array.from(radioInput).filter((radioBtn) => radioBtn.checked);
 
 
-    if (firstName.value.length < 2)                                  
-    { 
-      smallFirst.innerHTML = "Veuillez entrer 2 caractères ou plus pour le champ prénom.";
-      firstName.classList.add("error");
-      firstName.focus(); 
-      return false; 
-    }
-    if (lastName.value.length < 2)                                  
-    { 
-      smallFirst.innerHTML = "";
-      smallLast.innerHTML = "Veuillez entrer 2 caractères ou plus pour le champ nom.";
-      lastName.classList.add("error");
-      firstName.classList.remove("error");
-      lastName.focus(); 
-      return false; 
-    }   
-    
+function validName() {
+  let firstName = document.getElementById('first');
+  let smallFirst = firstName.nextElementSibling;
 
-    if (email.value == "")                                   
-    {   
-      smallFirst.innerHTML = "";
-      smallLast.innerHTML = "";
-      smallEmail.innerHTML= "Vous devez entrer une adresse email valide."; 
-      email.focus(); 
-      return false; 
-    }    
-    if (email.value.indexOf("@", 0) < 0)                 
-    { 
-      smallFirst.innerHTML = "";
-      smallLast.innerHTML = "";
-      smallEmail.innerHTML= "Vous devez entrer une adresse email valide."; 
-      email.focus(); 
-      return false; 
-    }    
-    if (email.value.indexOf(".", 0) < 0)                 
-    { 
-      smallFirst.innerHTML = "";
-      smallLast.innerHTML = "";
-      smallEmail.innerHTML = "Vous devez entrer une adresse email valide."; 
-      email.focus(); 
-      return false; 
-    }    
-    if (birthdate.value == "")                           
-    { 
-      smallFirst.innerHTML = "";
-      smallLast.innerHTML = "";
-      smallEmail.innerHTML = "";
-      smallBirthdate.innerHTML = "Vous devez entrer votre date de naissance."; 
-      birthdate.focus(); 
-      return false; 
-    }    
-    if (quantity.value == "")                        
-    { 
-      smallFirst.innerHTML = "";
-      smallLast.innerHTML = "";
-      smallEmail.innerHTML = "";
-      smallBirthdate.innerHTML = "";
-      smallQuantity.innerHTML = "Vous devez mettre une valeur."; 
-      quantity.focus(); 
-      return false; 
-    }  
-    if (!isRadioChecked || isRadioChecked.length <= 0) 
-    {
-      smallFirst.innerHTML = "";
-      smallLast.innerHTML = "";
-      smallEmail.innerHTML = "";
-      smallBirthdate.innerHTML = "";
-      smallQuantity.innerHTML = "";
-      smallCheckboxCity.innerHTML = "Sélectionnez une ville"; 
-      checkbox.focus()
-      return false;
-    }  
-    if (!checkbox.checked) 
-    {
-      smallFirst.innerHTML = "";
-      smallLast.innerHTML = "";
-      smallEmail.innerHTML = "";
-      smallBirthdate.innerHTML = "";
-      smallQuantity.innerHTML = "";
-      smallCheckboxCity.innerHTML = "";
-      smallCheckbox.innerHTML = "Vérifier que vous avez accepter les termes et conditions."; 
-      checkbox.focus()
-      return false;
-    }
+  if (firstName.value.length < 2)                                  
+  { 
+    smallFirst.innerHTML = "Veuillez entrer 2 caractères ou plus pour le champ prénom.";
+    firstName.classList.add("error");
+    firstName.focus(); 
+    result = 'error';
+  } else {
+    result = 'success';
+  }
+  return result;
+};
 
+console.log(validName());
 
-return true;
-     
-}
