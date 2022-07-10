ba1836cb16098384f59a0e91986d3f834e63045b
jeremy koundour <j.koundour@gmail.com>
2022-07-10 21:58:45 +0200

    finalisation de la validation et mise en place de la fenetre mercigit status(WIP: verification)

--- GameOn-website-FR/starterOnly/modal.js
+++ GameOn-website-FR/starterOnly/modal.js
@@ -36,23 +36,119 @@ closeBtn.forEach((btn) => btn.addEventListener("click", launchModal2))
 //let valid =true;
 
 
+// annonce des variables pour le formulaire
+let firstName = document.getElementById('first');
+let lastName = document.getElementById('last');               
+let email = document.getElementById('email');      
+let birthdate=  document.getElementById('birthdate');  
+let quantity= document.getElementById('quantity'); 
+let radioInput = document.querySelectorAll('.radio-input') 
+let checkbox= document.getElementById('checkbox1');
+let smallFirst = firstName.nextElementSibling;
+let smallLast = lastName.nextElementSibling;
+let smallEmail = email.nextElementSibling;
+let smallBirthdate = birthdate.nextElementSibling;
+let smallQuantity = quantity.nextElementSibling;
+let smallCheckbox = document.getElementById('errorCheckbox');
+let smallCheckboxCity = document.getElementById('errorCheckboxCity')
 
 
-function validName() {
-  let firstName = document.getElementById('first');
-  let smallFirst = firstName.nextElementSibling;
+// annonce des variables pour fin du formulaire
+let merci = document.getElementById("merci");
+let body = document.getElementById("mBody")
+console.log(merci);
 
-  if (firstName.value.length < 2)                                  
-  { 
-    smallFirst.innerHTML = "Veuillez entrer 2 caractères ou plus pour le champ prénom.";
+// annonce de la fonction de validation du formulaire
+// blocage de l'action de soumission de formulaire
+document.getElementById("boutonParti").addEventListener("click",(e) => {
+  e.preventDefault();
+
+  // validation Prénom
+  if (firstName.value.length < 2) {
     firstName.classList.add("error");
-    firstName.focus(); 
-    result = 'error';
-  } else {
-    result = 'success';
-  }
-  return result;
-};
+    smallFirst.innerHTML = "Veuillez entrer 2 caractères ou plus pour le champ prénom.";
+  }else{
+    smallFirst.innerHTML = "";
+    firstName.classList.remove("error");
+    var valid = true;
+    console.log(valid);
+  };
 
-console.log(validName());
+  // validation nom
+  if (lastName.value.length < 2) {
+    lastName.classList.add("error");
+    smallLast.innerHTML = "Veuillez entrer 2 caractères ou plus pour le champ nom.";
+  }else{
+    lastName.classList.remove("error");
+    smallLast.innerHTML = "";
+    var valid1 = true;
+    console.log(valid1);
+  };
+
+  // validation email
+  if (email.value == "") {
+    email.classList.add("error");
+    smallEmail.innerHTML= "Vous devez entrer une adresse email valide.";
+  }else if (email.value.indexOf("@", 0) < 0) {
+    email.classList.add("error");
+    smallEmail.innerHTML= "Vous devez entrer une adresse email valide.";
+  }else if (email.value.indexOf(".", 0) < 0) {
+    email.classList.add("error");
+    smallEmail.innerHTML= "Vous devez entrer une adresse email valide.";    
+  }else{
+    email.classList.remove("error");
+    smallEmail.innerHTML = "";
+    var valid2 = true;
+    console.log(valid2);
+  };
+
+  // validation date de naissance
+  if (birthdate.value == "") {
+      birthdate.classList.add("error");
+      smallBirthdate.innerHTML = "Vous devez entrer votre date de naissance.";
+  }else{
+      birthdate.classList.remove("error");
+      smallBirthdate.innerHTML = "";
+      var valid3 = true;
+      console.log(valid3);
+  };
+
+  // validation quantité
+  if (quantity.value == "") {
+    quantity.classList.add("error");
+    smallQuantity.innerHTML = "Vous devez mettre une valeur.";
+  }else{
+    quantity.classList.remove("error");
+    smallQuantity.innerHTML = "";
+    var valid4 = true;
+    console.log(valid4);
+  };
+
+  // validation ville
+  const isRadioChecked = Array.from(radioInput).filter(
+    (radioBtn) => radioBtn.checked
+  );
+  if (!isRadioChecked || isRadioChecked.length <= 0) {
+    smallCheckboxCity.innerHTML = "Sélectionnez une ville";
+  }else{
+    smallCheckboxCity.innerHTML = "";
+    var valid5 = true;
+    console.log(valid5);
+  };
+
+  // validation condition d'utilisation
+  if (!checkbox.checked) {
+    smallCheckbox.innerHTML = "Vérifier que vous avez accepter les termes et conditions.";
+  }else{
+    smallCheckbox.innerHTML = "";
+    var valid6 = true;
+    console.log(valid5);
+  };
+
+  // lancement page merci
+  if (valid==true && valid1==true && valid2==true && valid3==true && valid4==true && valid5==true && valid6==true){
+    merci.style.display ="block";
+    body.style.display ="none";
+  }
+});
 
